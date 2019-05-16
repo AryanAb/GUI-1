05219f545dd1dfdb34d7dde5ba74680893cafbbc
AryanAb <47926823+AryanAb@users.noreply.github.com>
2019-05-16 00:02:30 -0400

    Add files via upload

--- Frame2.java
+++ Frame2.java
@@ -16,30 +16,46 @@ public class Frame2 {
     private JPanel controlPanel8;
     private JPanel controlPanel9;
     private JPanel controlPanel10;
+    private JPanel controlPanel11;
 
     private JTextField t1 = new JTextField("Type your answer here");
+    private JTextField t2 = new JTextField("Type your answer here");
 
     private boolean isCorrect = false;
 
-    private JPanel[] panels = new JPanel[10];
+    private JPanel[] panels = new JPanel[11];
 
     private String[] questions = {"1. How do you make effective passwords?",
             "2. How can you get spyware?",
             "<html>3. Spyware is an unwanted software that infiltrates your computing device, stearing your internet usage data and sensitive information</html>",
     "<html>4. A ________ is a type of surveillance technology used to monitor and record each keystroke typed on a specific computer's keyboard.</html>",
-            "5. "};
+            "5. There is no need to read the term and condition.",
+    "6. How can you prevent spyware?",
+    "7. Piracy is unethical",
+    "<html>8. If you receive calls from collection agencies or creditors for an account you don't have, you might be a victim of ___________.</html>",
+    "9. How can you identify if the email you received is a phishing email?",
+    "10. Which of the following is a good password?",
+    "<html><i>11. Your Account is Suspended. We will ask for your password only once. Ignoring this message can cause eBay TKO delete your account forever.</i> What kind of unethical computer use is this?</html>"
+    };
 
     private int panelIndex = 0;
 
-    String textValue= "";
+    //String textValue= "";
 
-    JButton _continue = new JButton("Continue");
-    JButton _continue2 = new JButton("Continue");
-    JButton _continue3 = new JButton("Continue");
-    JButton _continue4 = new JButton("Continue");
-    JButton _continue5 = new JButton("Continue");
+    private JButton _continue = new JButton("Continue");
+    private JButton _continue2 = new JButton("Continue");
+    private JButton _continue3 = new JButton("Continue");
+    private JButton _continue4 = new JButton("Continue");
+    private JButton _continue5 = new JButton("Continue");
+    private JButton _continue6 = new JButton("Continue");
+    private JButton _continue7 = new JButton("Continue");
+    private JButton _continue8 = new JButton("Continue");
+    private JButton _continue9 = new JButton("Continue");
+    private JButton _continue10 = new JButton("Continue");
+    private JButton _continue11 = new JButton("Continue");
 
-    private JButton[] continueButtons = {_continue, _continue2, _continue3, _continue4, _continue5};
+    private JButton[] continueButtons = {_continue, _continue2, _continue3, _continue4, _continue5, _continue6, _continue7, _continue8,
+            _continue9, _continue10, _continue11};
 
 
     public Frame2() {
@@ -127,6 +143,11 @@ public class Frame2 {
         controlPanel10.setLayout(new BoxLayout(controlPanel10, BoxLayout.PAGE_AXIS));
         controlPanel10.setVisible(false);
         panels[9] = controlPanel10;
+
+        controlPanel11 = new JPanel();
+        controlPanel11.setLayout(new BoxLayout(controlPanel11, BoxLayout.PAGE_AXIS));
+        controlPanel11.setVisible(false);
+        panels[10] = controlPanel11;
 
         mainFrame.add(headerLabel);
         mainFrame.add(controlPanel);
@@ -298,7 +319,6 @@ public class Frame2 {
 
         //======================================Question 4===============================================
 
-        //JTextField t1 = new JTextField("Type your answer here");
         t1.setMaximumSize(new Dimension(500, 45));
 
         JButton submit4 = new JButton("Submit");
@@ -313,25 +333,340 @@ public class Frame2 {
         submit4.addActionListener(new ButtonClickListener());
         _continue4.addActionListener(new ButtonClickListener());
 
-        textValue = t1.getText();
-
         //===============================================================================================
 
         //======================================Question 5===============================================
 
-        JTextField t2 = new JTextField("Type your answer here");
-        t2.setMaximumSize(new Dimension(750, 45));
+        String _trueString1 = "True ";
+        String _falseString1 = "False ";
+
+        JRadioButton _true2 = new JRadioButton(_trueString1);
+        _true2.setMnemonic(KeyEvent.VK_B);
+        _true2.setActionCommand(_trueString1);
+
+        JRadioButton _false2 = new JRadioButton(_falseString1);
+        _false2.setMnemonic(KeyEvent.VK_D);
+        _false2.setActionCommand(_falseString1);
 
         JButton submit5 = new JButton("Submit");
 
-        controlPanel5.add(t2);
+        controlPanel5.add(_true2);
+        controlPanel5.add(_false2);
 
         controlPanel5.add(submit5);
 
         _continue5.setEnabled(false);
         controlPanel5.add(_continue5);
 
+        ButtonGroup group5 = new ButtonGroup();
+        group5.add(_true2);
+        group5.add(_false2);
 
+        _true2.addActionListener(new ButtonClickListener());
+        _false2.addActionListener(new ButtonClickListener());
+        submit5.addActionListener(new ButtonClickListener());
+        _continue5.addActionListener(new ButtonClickListener());
+
+
+        //===============================================================================================
+
+        //============================================Question 6=========================================
+
+        String b11String = "Don't open emails from unknown senders";
+        String b12String = "Don't download files from untrustworthy sources";
+        String b13String = "Don't click pop-op advertisements";
+        String b14String = "Use reputable antivirus software";
+        String b15String = "All of the above";
+
+        JRadioButton b11 = new JRadioButton(b11String);
+        b11.setMnemonic(KeyEvent.VK_B);
+        b11.setActionCommand(b11String);
+        b11.setSelected(true);
+
+        JRadioButton b12 = new JRadioButton(b12String);
+        b12.setMnemonic(KeyEvent.VK_C);
+        b12.setActionCommand(b12String);
+
+        JRadioButton b13 = new JRadioButton(b13String);
+        b13.setMnemonic(KeyEvent.VK_D);
+        b13.setActionCommand(b13String);
+
+        JRadioButton b14 = new JRadioButton(b14String);
+        b14.setMnemonic(KeyEvent.VK_R);
+        b14.setActionCommand(b14String);
+
+        JRadioButton b15 = new JRadioButton(b15String);
+        b15.setMnemonic(KeyEvent.VK_E);
+        b15.setActionCommand(b15String);
+
+
+        JButton submit6 = new JButton("Submit");
+
+
+        controlPanel6.add(b11);
+        controlPanel6.add(b12);
+        controlPanel6.add(b13);
+        controlPanel6.add(b14);
+        controlPanel6.add(b15);
+
+        controlPanel6.add(submit6);
+
+        _continue6.setEnabled(false);
+        controlPanel6.add(_continue6);
+
+        ButtonGroup group6 = new ButtonGroup();
+        group6.add(b11);
+        group6.add(b12);
+        group6.add(b13);
+        group6.add(b14);
+        group6.add(b15);
+
+
+        b11.addActionListener(new ButtonClickListener());
+        b12.addActionListener(new ButtonClickListener());
+        b13.addActionListener(new ButtonClickListener());
+        b14.addActionListener(new ButtonClickListener());
+        b15.addActionListener(new ButtonClickListener());
+        submit6.addActionListener(new ButtonClickListener());
+        _continue6.addActionListener(new ButtonClickListener());
+
+        //===============================================================================================
+
+        //=============================================Question 7========================================
+
+        //String _trueString1 = "True ";
+        //String _falseString1 = "False ";
+
+        JRadioButton _true3 = new JRadioButton(_trueString1);
+        _true3.setMnemonic(KeyEvent.VK_B);
+        _true3.setActionCommand(_trueString);
+
+        JRadioButton _false3 = new JRadioButton(_falseString1);
+        _false3.setMnemonic(KeyEvent.VK_D);
+        _false3.setActionCommand(_falseString);
+
+        JButton submit7 = new JButton("Submit");
+
+        controlPanel7.add(_true3);
+        controlPanel7.add(_false3);
+
+        controlPanel7.add(submit7);
+
+        _continue7.setEnabled(false);
+        controlPanel7.add(_continue7);
+
+        ButtonGroup group7 = new ButtonGroup();
+        group7.add(_true3);
+        group7.add(_false3);
+
+        _true3.addActionListener(new ButtonClickListener());
+        _false3.addActionListener(new ButtonClickListener());
+        submit7.addActionListener(new ButtonClickListener());
+        _continue7.addActionListener(new ButtonClickListener());
+
+        //===============================================================================================
+
+        //============================================Question 8=========================================
+
+        t2.setMaximumSize(new Dimension(500, 45));
+
+        JButton submit8 = new JButton("Submit");
+
+        controlPanel8.add(t2);
+
+        controlPanel8.add(submit8);
+
+        _continue8.setEnabled(false);
+        controlPanel8.add(_continue8);
+
+        submit8.addActionListener(new ButtonClickListener());
+        _continue8.addActionListener(new ButtonClickListener());
+
+        //===============================================================================================
+
+        //============================================Question 9=========================================
+
+        String b16String = "Wrong company name";
+        String b17String = "Spelling or grammar mistakes";
+        String b18String = "Questionable deadlines";
+        String b19String = "Suspicious links";
+        String b20String = "All of the above";
+
+        JRadioButton b16 = new JRadioButton(b16String);
+        b16.setMnemonic(KeyEvent.VK_B);
+        b16.setActionCommand(b16String);
+        b16.setSelected(true);
+
+        JRadioButton b17 = new JRadioButton(b17String);
+        b17.setMnemonic(KeyEvent.VK_C);
+        b17.setActionCommand(b17String);
+
+        JRadioButton b18 = new JRadioButton(b18String);
+        b18.setMnemonic(KeyEvent.VK_D);
+        b18.setActionCommand(b18String);
+
+        JRadioButton b19 = new JRadioButton(b19String);
+        b19.setMnemonic(KeyEvent.VK_R);
+        b19.setActionCommand(b19String);
+
+        JRadioButton b20 = new JRadioButton(b20String);
+        b20.setMnemonic(KeyEvent.VK_E);
+        b20.setActionCommand(b20String);
+
+
+        JButton submit9 = new JButton("Submit");
+
+
+        controlPanel9.add(b16);
+        controlPanel9.add(b17);
+        controlPanel9.add(b18);
+        controlPanel9.add(b19);
+        controlPanel9.add(b20);
+
+        controlPanel9.add(submit9);
+
+        _continue9.setEnabled(false);
+        controlPanel9.add(_continue9);
+
+        ButtonGroup group9 = new ButtonGroup();
+        group9.add(b16);
+        group9.add(b17);
+        group9.add(b18);
+        group9.add(b19);
+        group9.add(b20);
+
+
+        b16.addActionListener(new ButtonClickListener());
+        b17.addActionListener(new ButtonClickListener());
+        b18.addActionListener(new ButtonClickListener());
+        b19.addActionListener(new ButtonClickListener());
+        b20.addActionListener(new ButtonClickListener());
+        submit9.addActionListener(new ButtonClickListener());
+        _continue9.addActionListener(new ButtonClickListener());
+
+        //===============================================================================================
+
+        //===========================================Question 10=========================================
+
+        String b21String = "ilovecompsci";
+        String b22String = "gh1ghiph!@iAhg2!";
+        String b23String = "qwerty";
+        String b24String = "c0mpsc!";
+        String b25String = "p@55word";
+
+        JRadioButton b21 = new JRadioButton(b21String);
+        b21.setMnemonic(KeyEvent.VK_B);
+        b21.setActionCommand(b16String);
+        b21.setSelected(true);
+
+        JRadioButton b22 = new JRadioButton(b22String);
+        b22.setMnemonic(KeyEvent.VK_C);
+        b22.setActionCommand(b17String);
+
+        JRadioButton b23 = new JRadioButton(b23String);
+        b23.setMnemonic(KeyEvent.VK_D);
+        b23.setActionCommand(b18String);
+
+        JRadioButton b24 = new JRadioButton(b24String);
+        b24.setMnemonic(KeyEvent.VK_R);
+        b24.setActionCommand(b19String);
+
+        JRadioButton b25 = new JRadioButton(b25String);
+        b25.setMnemonic(KeyEvent.VK_E);
+        b25.setActionCommand(b20String);
+
+
+        JButton submit10 = new JButton("Submit");
+
+
+        controlPanel10.add(b21);
+        controlPanel10.add(b22);
+        controlPanel10.add(b23);
+        controlPanel10.add(b24);
+        controlPanel10.add(b25);
+
+        controlPanel10.add(submit10);
+
+        _continue10.setEnabled(false);
+        controlPanel10.add(_continue10);
+
+        ButtonGroup group10 = new ButtonGroup();
+        group10.add(b21);
+        group10.add(b22);
+        group10.add(b23);
+        group10.add(b24);
+        group10.add(b25);
+
+
+        b21.addActionListener(new ButtonClickListener());
+        b22.addActionListener(new ButtonClickListener());
+        b23.addActionListener(new ButtonClickListener());
+        b24.addActionListener(new ButtonClickListener());
+        b25.addActionListener(new ButtonClickListener());
+        submit10.addActionListener(new ButtonClickListener());
+        _continue10.addActionListener(new ButtonClickListener());
+
+        //===============================================================================================
+
+        //========================================Question 11============================================
+
+        String b26String = "Phishing";
+        String b27String = "Keystorke logging";
+        String b28String = "Cyberbullying";
+        String b29String = "None of the above";
+        String b30String = "All of the above ";
+
+        JRadioButton b26 = new JRadioButton(b26String);
+        b26.setMnemonic(KeyEvent.VK_B);
+        b26.setActionCommand(b16String);
+        b26.setSelected(true);
+
+        JRadioButton b27 = new JRadioButton(b27String);
+        b27.setMnemonic(KeyEvent.VK_C);
+        b27.setActionCommand(b17String);
+
+        JRadioButton b28 = new JRadioButton(b28String);
+        b28.setMnemonic(KeyEvent.VK_D);
+        b28.setActionCommand(b18String);
+
+        JRadioButton b29 = new JRadioButton(b29String);
+        b29.setMnemonic(KeyEvent.VK_R);
+        b29.setActionCommand(b19String);
+
+        JRadioButton b30 = new JRadioButton(b30String);
+        b30.setMnemonic(KeyEvent.VK_E);
+        b30.setActionCommand(b20String);
+
+
+        JButton submit11 = new JButton("Submit");
+
+
+        controlPanel11.add(b26);
+        controlPanel11.add(b27);
+        controlPanel11.add(b28);
+        controlPanel11.add(b29);
+        controlPanel11.add(b30);
+
+        controlPanel11.add(submit11);
+
+        _continue11.setEnabled(false);
+        controlPanel11.add(_continue11);
+
+        ButtonGroup group11 = new ButtonGroup();
+        group11.add(b26);
+        group11.add(b27);
+        group11.add(b28);
+        group11.add(b29);
+        group11.add(b30);
+
+
+        b26.addActionListener(new ButtonClickListener());
+        b27.addActionListener(new ButtonClickListener());
+        b28.addActionListener(new ButtonClickListener());
+        b29.addActionListener(new ButtonClickListener());
+        b30.addActionListener(new ButtonClickListener());
+        submit11.addActionListener(new ButtonClickListener());
+        _continue11.addActionListener(new ButtonClickListener());
 
         //===============================================================================================
 
@@ -342,35 +677,49 @@ public class Frame2 {
 
             String command = e.getActionCommand();
 
+            if(command.equals("Submit")){
+                if(t1.getText().toLowerCase().equals("keylogger")){
+                    isCorrect = true;
+                } else if(t2.getText().toLowerCase().equals("identity theft")){
+                    isCorrect = true;
+                }
+            }
             if (command.equals("All of the above")) {
                 isCorrect = true;
                 //System.out.println("Correct Radio Button Selected");
             } else if(command.equals("True")){
-                //System.out.println("Correct Answer Selected");
+                System.out.println("Correct Answer Selected");
                 isCorrect = true;
             }
-            //if(textValue.toLowerCase().equals("blank")){
-              //  isCorrect = true;
-                //System.out.println("You typed the correct answer");
-            //}
-            if(!command.equals("All of the above") && !command.equals("Submit") && !command.equals("True")){
+            if(command.equals("False ")){
+                isCorrect = true;
+            }else if(command.equals("True ")) {
                 isCorrect = false;
+            }
+            if(command.equals("gh1ghiph!@iAhg2!")){
+                isCorrect = true;
+            }
+            if(command.equals("Phishing")){
+                isCorrect = true;
+            }
+            // +++++++++++++++++++++++++++++++++++++++++++fix this++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            //if(!command.equals("All of the above") && !command.equals("Submit") && !command.equals("True")){
+              //  isCorrect = false;
                 //System.out.println("Wrong Answer Selected");
-            }
-            if(command.equals("Submit")){
-                textValue = t1.getText();
-                if(textValue.toLowerCase().equals("keylogger")){
-                    isCorrect = true;
-                }
-            }
+            //}
             if (command.equals("Submit") && isCorrect) {
+                mainFrame.add(statusLabel);
                 statusLabel.setText("Correct!");
+                mainFrame.repaint();
                 continueButtons[panelIndex].setEnabled(true);
                 //System.out.println("Correct!");
             } else if (command.equals("Submit") && !isCorrect) {
+                mainFrame.add(statusLabel);
                 statusLabel.setText("Wrong!");
+                mainFrame.repaint();
             }
             if (command.equals("Continue")) {
+                t1.setText("Enter your answer here");
                 mainFrame.remove(statusLabel);
                 mainFrame.repaint();
                 headerLabel.setText(questions[panelIndex + 1]);
